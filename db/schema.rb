e6610596b49e767dfa2fc04134b0f79f33af018d
KayKawa <goodenoughliving1221@gmail.com>
2022-06-21 15:48:55 +0900

    動物5種のScaffoldを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_064414) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_064825) do
   create_table "cats", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chicks", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hedgehogs", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owls", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
